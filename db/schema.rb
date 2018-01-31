01a619c4a9de25681381a4ac49d0481bdfcd9731
Rivelino Junior <rv.fernandes.junior@gmail.com>
2018-01-30 23:27:18 -0400

    generate migration for create table companies

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180131031722) do
+ActiveRecord::Schema.define(version: 20180131032057) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "companies", force: :cascade do |t|
+    t.string "name"
+  end
 
   create_table "faq_hashtags", force: :cascade do |t|
     t.integer "faq_id"
