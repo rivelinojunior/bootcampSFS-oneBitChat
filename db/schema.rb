b9f3c029e1f63791d3f29af203e237eb745d08df
Rivelino Junior <rv.fernandes.junior@gmail.com>
2018-01-30 23:19:11 -0400

    generate migrate for create table faq_hashtags

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180131031224) do
+ActiveRecord::Schema.define(version: 20180131031722) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "faq_hashtags", force: :cascade do |t|
+    t.integer "faq_id"
+    t.integer "hashtag_id"
+  end
 
   create_table "faqs", force: :cascade do |t|
     t.string "question"
