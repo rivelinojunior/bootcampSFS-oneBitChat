e5cfc2c38177691d3b7ad8a46f6b392bdaa520ef
Rivelino Junior <rv.fernandes.junior@gmail.com>
2018-02-01 13:16:06 -0400

    create test to faq remove service

---
+++ spec/services/faq/remove_service_spec.rb
@@ -0,0 +1,33 @@
+require_relative './../../spec_helper.rb'
+
+describe FaqModule::RemoveService do
+  before do
+    @company = create(:company)
+  end
+
+  describe '#call' do
+    context "With valid ID" do
+      before do 
+        faq = create(:faq, company: @company)
+        @removeService = FaqModule::RemoveService.new({"id" => faq.id})
+        @response = @removeService.call()
+      end
+
+      it "message remove Faq " do
+        expect(response).to match("Deletado com sucesso")
+      end
+
+      it "remove Faq from database" do
+        expect(Faq.all.count).to eq(0)
+      end
+    end
+
+    context "With invalid ID" do
+      it "receive error message" do
+        @removeService = FaqModule::RemoveService.new({"id" => rand(1..9999)})
+        response = @removeService.call()
+        expect(response).to match("Questão inválida, verifique o Id")
+      end
+    end 
+   end
+end
