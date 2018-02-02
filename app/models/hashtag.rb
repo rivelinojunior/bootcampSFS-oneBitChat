29e2d110ddf69c1b12a05703d9548f0ea3eab520
Rivelino Junior <rv.fernandes.junior@gmail.com>
2018-02-02 16:17:10 -0400

    add relationship of hashtag with faq_hashtags/faqs

--- app/models/hashtag.rb
+++ app/models/hashtag.rb
@@ -1,4 +1,8 @@
 class Hashtag < ActiveRecord::Base
   validates_presence_of :name
+  
+  has_many :faq_hashtags, dependent: :destroy
+  has_many :faqs, through: :faq_hashtags
+
   belongs_to :company
 end
