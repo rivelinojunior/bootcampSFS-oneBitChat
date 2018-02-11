8191206c9cbfa8170d73457ef7d7477445ed3b13
Rivelino Junior <rv.fernandes.junior@gmail.com>
2018-02-10 20:00:27 -0400

    include pg_search in model link

--- app/models/link.rb
+++ app/models/link.rb
@@ -1,7 +1,12 @@
+require "pg_search"
+include PgSearch
+
 class Link < ActiveRecord::Base
   
   belongs_to :company
   validates :company, presence: { message: "Obrigatória" }
   validates :url, presence: { message: "Obrigatória" }
   validates :subject, presence: { message: "Obrigatório" }
+
+  pg_search_scope :search, :against => [:subject, :url]
 end
